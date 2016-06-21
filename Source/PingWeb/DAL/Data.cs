a526baa65443775c803e1890ff477d44ec5e0c15
Jemin <jemsworld@gmail.com>
2016-06-21 21:39:11 +0100

    Initial commit
    Initial commit after folder restructuring

---
+++ Source/PingWeb/DAL/Data.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.SQLite;
+
+namespace HttpRequestTester.DAL
+{
+    using HttpRequestTester.Model;
+
+    public class Data
+    {
+        private static readonly SQLiteConnection DbConnection = new SQLiteConnection("Data Source=Database.sqlite;Version=3;");
+
+        public static void CreateDatabase()
+        {
+            DbConnection.Open();
+
+            const string SqlCreateTable = @"CREATE TABLE IF NOT EXISTS  Request 
+                                    (RequestId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                                    RequestUrl VARCHAR(100), 
+                                    Response VARCHAR(100), 
+                                    DateRequest DATETIME, 
+                                    ResponseTime INT, 
+                                    IsSuccess BOOLEAN)";
+
+            var command = new SQLiteCommand(SqlCreateTable, DbConnection);
+            command.ExecuteNonQuery();
+            
+            DbConnection.Close();
+        }
+
+        public static void SaveToDb(HttpGet data)
+        {
+            DbConnection.Open();
+
+            var sqlInsert =
+                string.Format(
+                    @"INSERT INTO Request 
+                        (RequestUrl, Response, DateRequest, ResponseTime, IsSuccess) 
+                        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}' )",
+                    data.Url,
+                    (data.Response == null ? string.Empty : data.Response.Replace("'", "''")),
+                    data.RequestDateTime,
+                    data.ResponseTime,
+                    data.IsSuccess);
+
+            var command = new SQLiteCommand(sqlInsert, DbConnection);
+            command.ExecuteNonQuery();
+
+            DbConnection.Close();
+        }
+
+
+    }
+}
---
+++ Source/PingWeb/Model/HttpGet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HttpRequestTester.Model
+{
+    public class HttpGet
+    {
+        public string Url { get; set; }
+
+        public string Response { get; set; }
+
+        public DateTime RequestDateTime { get; set; }
+
+        public DateTime ResponseDateTime { get; set; }
+
+        public int? ResponseTime { get; set; }
+
+        public bool IsSuccess { get; set; }
+    }
+}
